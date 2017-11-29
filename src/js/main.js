3fe29900cdd910756d5ba68a1b2eb38f0583ac48
Gonzalo Alba Durán <gonalba@ucm.es>
2017-11-29 13:58:13 +0100

    mov_horiz2

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,84 @@
+/*
+//var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+'use strict';
+//variables que contienen los demas scripts
+var PlayScene = require('./play_scene.js');
+
+
+
+var PreloaderScene = {
+	preload: function () {
+		this.game.load.image('oso', '../images/oso.png');
+	},
+	create: function() {
+		this.game.state.start('playScene');
+	}
+};
+
+window.onload = function () {
+	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game');
+  //TODO 1.2 Añadir los states 'boot' BootScene, 'menu, 'preloader' PreloaderScene, 'play' PlayScene, 'gameOver' GameOver.
+	game.state.add('boot', BootScene);
+	game.state.add('playScene', MenuScene);
+	game.state.add('preloader', PreloaderScene);
+
+
+	//TODO 1.3 iniciar el state 'boot'. 
+	game.state.start('preloader');
+};
+*/
+/*
+'use strict';
+
+var PlayScene = require('./play_scene.js');
+
+
+var BootScene = {
+  preload: function () {
+    // load here assets required for the loading screen
+    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+  },
+
+  create: function () {
+    this.game.state.start('preloader');
+  }
+};
+
+
+var PreloaderScene = {
+  preload: function () {
+    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
+    this.loadingBar.anchor.setTo(0, 0.5);
+    this.load.setPreloadSprite(this.loadingBar);
+
+    // TODO: load here the assets for the game
+    this.game.load.image('oso', 'images/oso.png');
+  },
+
+  create: function () {
+    this.game.state.start('play');
+  }
+};
+
+
+window.onload = function () {
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+
+  game.state.add('boot', BootScene);
+  game.state.add('preloader', PreloaderScene);
+  game.state.add('play', PlayScene);
+
+  game.state.start('boot');
+};
+
+*/
 
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.image('atari', '../images/oaso.png');
-    game.load.image('mushroom', '../images/osoa.png');
+    game.load.image('atari', 'assets/sprites/atari130xe.png');
+    game.load.image('mushroom', 'assets/sprites/mushroom2.png');
 
 }
 
@@ -17,18 +91,17 @@ function create() {
 
     game.stage.backgroundColor = '#2d2d2d';
 
-    sprite1 = game.add.sprite(400, 50, 'atari');
+    sprite1 = game.add.sprite(300, 50, 'atari');
     sprite2 = game.add.sprite(400, 450, 'mushroom');
 
-    game.physics.arcade.enable([ sprite1, sprite2 ], Phaser.Physics.ARCADE);
+    game.physics.arcade.enable(sprite1, Phaser.Physics.ARCADE);
 
-    //game.add.tween(sprite1.body).to( { y: 450 }, 3000, Phaser.Easing.Linear.None, true);
+    //game.add.tween(sprite1.body).to( { y: 400 }, 3000, Phaser.Easing.Linear.None, true);
 
 }
 
 function update() {
-
-	//sprite1.body.velocity.x = -150;
+	sprite1.body.velocity.x = -150;
     game.physics.arcade.overlap(sprite1, sprite2, overlapHandler, null, this);
 
 }
@@ -43,7 +116,7 @@ function overlapHandler (obj1, obj2) {
 
 function render() {
 
-   // game.debug.body(sprite1);
-   // game.debug.body(sprite2);
+    game.debug.body(sprite1);
+    game.debug.body(sprite2);
 
 }
--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,13 +1,42 @@
-'use strict';
+/*'use strict';
 
 var PlayScene = {
   	create: function () {
-   		var oso = this.game.add.sprite(
-      	this.game.world.centerX, this.game.world.centerY, 'oso');
+   		var oso = this.game.add.sprite(600, 100, 'oso');
     	//oso.anchor.setTo(0.5, 0.5);
-    	oso.scale.setTo(0.75, 0.75);
+    	//oso.scale.setTo(0.75, 0.75);
     	
-    }    
+    	/*this.game.physics.startSystem(Phaser.Physics.ARCADE);
+	    this.game.physics.arcade.enable(oso , Phaser.Physics.ARCADE);
+
+    	//game.add.tween(sprite1.body).to( { y: 450 }, 3000, Phaser.Easing.Linear.None, true);
+
+	},
+
+	update: function () {
+
+		//sprite1.body.velocity.x = -150;
+	    //this.game.physics.arcade.overlap(sprite1, sprite2, overlapHandler, null, this);
+
+	}
+};
+
+module.exports = PlayScene;*/
+
+'use strict';
+
+var PlayScene = {
+  create: function () {
+	this.game.physics.startSystem(Phaser.Physics.ARCADE);
+	this.game.physics.arcade.enable(oso , Phaser.Physics.ARCADE);
+	
+    var logo = this.game.add.sprite(
+      this.game.world.centerX, this.game.world.centerY, 'oso');
+    logo.anchor.setTo(0.5, 0.5);
+
+    
+  }
 };
 
 module.exports = PlayScene;
+*/
