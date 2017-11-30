64bfdf8de20414e1f348e980f7b95c6bce0ae5e8
Gonzalo Alba Durán <gonalba@ucm.es>
2017-11-30 13:08:50 +0100

    MovHoriz_Jump
    Implementacion del movimiento horizontal mediante las flechas de direccion y el salto mediante el espacio

--- src/js/main.js
+++ src/js/main.js
@@ -75,15 +75,21 @@ window.onload = function () {
 
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
+
+
 function preload() {
 
-    game.load.image('atari', 'assets/sprites/atari130xe.png');
-    game.load.image('mushroom', 'assets/sprites/mushroom2.png');
+    game.load.image('atari', '../images/oso.png');
+    //game.load.image('mushroom', 'assets/sprites/mushroom2.png');
 
 }
 
-var sprite1;
-var sprite2;
+var oso;
+var cursors;
+var jumpButton;
+var jumpTimer = 0;
+
+//var sprite2;
 
 function create() {
 
@@ -91,32 +97,45 @@ function create() {
 
     game.stage.backgroundColor = '#2d2d2d';
 
-    sprite1 = game.add.sprite(300, 50, 'atari');
-    sprite2 = game.add.sprite(400, 450, 'mushroom');
+    game.physics.arcade.gravity.y = 500;
 
-    game.physics.arcade.enable(sprite1, Phaser.Physics.ARCADE);
+    oso = game.add.sprite(300, 300, 'atari');
+   // sprite2 = game.add.sprite(400, 450, 'mushroom');
+    game.physics.arcade.enable(oso, Phaser.Physics.ARCADE);
+
+	oso.body.bounce.y = 0.2;
+    oso.body.collideWorldBounds = true;
+    oso.body.setSize(oso.width, oso.height);
 
     //game.add.tween(sprite1.body).to( { y: 400 }, 3000, Phaser.Easing.Linear.None, true);
+   	cursors = game.input.keyboard.createCursorKeys();
+   	jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 }
 
 function update() {
-	sprite1.body.velocity.x = -150;
-    game.physics.arcade.overlap(sprite1, sprite2, overlapHandler, null, this);
+	oso.body.velocity.x = 0;
+	if (cursors.left.isDown){
+		oso.body.velocity.x = -150;
+	}
+	else if(cursors.right.isDown){
+		oso.body.velocity.x = 150;
+	}
+
+	if (jumpButton.isDown && oso.body.onFloor())
+    {
+        oso.body.velocity.y = -450;
+        //jumpTimer = game.time.now + 750;
+    }
+   // game.physics.arcade.overlap(sprite1, sprite2, overlapHandler, null, this);			&& game.time.now > jumpTimer
 
 }
 
-function overlapHandler (obj1, obj2) {
 
-    game.stage.backgroundColor = '#992d2d';
-
-    obj2.kill();
-
-}
 
 function render() {
 
-    game.debug.body(sprite1);
-    game.debug.body(sprite2);
+   // game.debug.body(sprite1);
+    //game.debug.body(sprite2);
 
 }
