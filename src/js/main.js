ec987d860c07c2da16285c433623d4a8381b172c
Gonalba <gonalba@ucm.es>
2017-12-01 03:02:56 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -105,7 +105,7 @@ function create() {
 
     game.stage.backgroundColor = '#2d2d2d';
 
-    game.physics.arcade.gravity.y = 10000; // gravedad
+    game.physics.arcade.gravity.y = 9000; // gravedad
 //oso
     // añado el sprite del oso y le doy fisica
     oso = game.add.sprite(100, 300, 'oso');
@@ -135,15 +135,15 @@ function update() {
 
 	oso.body.velocity.x = 0;
 	if (cursors.left.isDown){
-		oso.body.velocity.x = -400;
+		oso.body.velocity.x = -300;
 	}
 	else if(cursors.right.isDown){
-		oso.body.velocity.x = 400;
+		oso.body.velocity.x = 300;
 	}
 
 	if ((jumpButton.isDown) && (oso.body.onFloor() || collisionPlatOso)) // si se a pulsado el espacio y esta en el suelo o en una plataforma
     {
-        oso.body.velocity.y = -2000;
+        oso.body.velocity.y = -1500;
         
     }
     collisionPlatOso = false;
