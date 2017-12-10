7f1ee071713a34ee7e4adfd24c676c8ce22c417b
Gonzalo Alba Durán <gonalba@ucm.es>
2017-12-10 19:57:40 +0100

    .

--- src/js/main.js
+++ src/js/main.js
@@ -40,4 +40,100 @@ window.onload = function () {
 
   game.state.start('boot');
 };
+<<<<<<< HEAD
+=======
+
+*/
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+
+
+function preload() {
+	
+	game.load.baseURL = 'https://gonalba.github.io/IceClimbers/src/';
+
+    game.load.crossOrigin = 'anonymous';
+
+    //game.load.image('phaser', 'sprites/phaser-dude.png');
+
+    game.load.image('oso', 'images/oso.png');
+    game.load.image('plataforma', 'images/preloader_bar.png');
+    //game.load.image('mushroom', 'assets/sprites/mushroom2.png');
+
+}
+var collisionPlatOso = false;
+var oso;
+var plataforma;
+var cursors;
+var jumpButton;
+var jumpTimer = 0;
+
+//var sprite2;
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE); //añado la fisica al juego
+
+    game.stage.backgroundColor = '#2d2d2d';
+
+    game.physics.arcade.gravity.y = 300; // gravedad
+//oso
+    // añado el sprite del oso y le doy fisica
+    oso = game.add.sprite(100, 300, 'oso');
+    oso.anchor.set(0.5);
+    oso.scale.set(0.5);
+   	game.physics.arcade.enable(oso, Phaser.Physics.ARCADE);
+	//oso.body.bounce.y = 0.3;  // para que al caer rebote en el suelo
+    oso.body.collideWorldBounds = true;	// el collider no sale de los limites del mundo
+
+//plataforma
+    // añado el sprite de la plataforma y le doy fisica
+    plataforma = game.add.sprite(300, 450, 'plataforma');
+   	game.physics.arcade.enable(plataforma, Phaser.Physics.ARCADE);
+    
+    plataforma.body.allowGravity = false;	//	la gravedad no le afecta
+    plataforma.body.immovable = true;	//	la plataforma es inmovible
+
+
+   	cursors = game.input.keyboard.createCursorKeys();
+   	jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+}
+
+function update() {
+	game.physics.arcade.collide(oso, plataforma,collisionTrue, null, this);//para que el oso se quede encima de la plataforma y no la atraviese
+	
+
+	oso.body.velocity.x = 0;
+	if (cursors.left.isDown){
+		oso.body.velocity.x = -250;
+	}
+	else if(cursors.right.isDown){
+		oso.body.velocity.x = 250;
+	}
+
+	if ((jumpButton.isDown) && (oso.body.onFloor() || collisionPlatOso)) // si se a pulsado el espacio y esta en el suelo o en una plataforma
+    {
+        oso.body.velocity.y = -1000;
+        
+    }
+    collisionPlatOso = false;
+
+}
+
+//metodo para saber si esta colisionando con una plataforma
+function collisionTrue (obj1, obj2) {
+
+    collisionPlatOso = true;
+
+}
+
+
+
+function render() {
+
+    /*game.debug.body(plataforma);
+    game.debug.body(oso);*/
+}
 >>>>>>> parent of 62124ff... calibracion gravedad
