1e0f93083e0174d74b54e6da833f37a9173bca64
Gonzalo Alba Durán <gonalba@ucm.es>
2017-12-10 20:45:09 +0100

    Revert "calibracion gravedad"
    This reverts commit 62124ff487ed201005c841fee5f0400b7087330c.

--- src/js/main.js
+++ src/js/main.js
@@ -94,8 +94,28 @@ function create() {
 
     game.stage.backgroundColor = '#2d2d2d';
 
+<<<<<<< HEAD
     sprite1 = game.add.sprite(300, 50, 'atari');
     sprite2 = game.add.sprite(400, 450, 'mushroom');
+=======
+    game.physics.arcade.gravity.y = 300; // gravedad
+//oso
+    // añado el sprite del oso y le doy fisica
+    oso = game.add.sprite(100, 300, 'oso');
+    oso.anchor.set(0.5);
+    oso.scale.set(0.5);
+   	game.physics.arcade.enable(oso, Phaser.Physics.ARCADE);
+	//oso.body.bounce.y = 0.3;  // para que al caer rebote en el suelo
+    oso.body.collideWorldBounds = true;	// el collider no sale de los limites del mundo
+
+//plataforma
+    // añado el sprite de la plataforma y le doy fisica
+    plataforma = game.add.sprite(300, 450, 'plataforma');
+   	game.physics.arcade.enable(plataforma, Phaser.Physics.ARCADE);
+    
+    plataforma.body.allowGravity = false;	//	la gravedad no le afecta
+    plataforma.body.immovable = true;	//	la plataforma es inmovible
+>>>>>>> parent of 62124ff... calibracion gravedad
 
     game.physics.arcade.enable(sprite1, Phaser.Physics.ARCADE);
 
@@ -104,8 +124,28 @@ function create() {
 }
 
 function update() {
+<<<<<<< HEAD
 	sprite1.body.velocity.x = -150;
     game.physics.arcade.overlap(sprite1, sprite2, overlapHandler, null, this);
+=======
+	game.physics.arcade.collide(oso, plataforma,collisionTrue, null, this);//para que el oso se quede encima de la plataforma y no la atraviese
+	
+
+	oso.body.velocity.x = 0;
+	if (cursors.left.isDown){
+		oso.body.velocity.x = -250;
+	}
+	else if(cursors.right.isDown){
+		oso.body.velocity.x = 250;
+	}
+
+	if ((jumpButton.isDown) && (oso.body.onFloor() || collisionPlatOso)) // si se a pulsado el espacio y esta en el suelo o en una plataforma
+    {
+        oso.body.velocity.y = -1000;
+        
+    }
+    collisionPlatOso = false;
+>>>>>>> parent of 62124ff... calibracion gravedad
 
 }
 
