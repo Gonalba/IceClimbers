2b48064c4f68299ea246d77b892135ff12049068
Celia <celica02@ucm.es>
2017-12-15 18:41:48 +0100

    Comienzo herencia
    Estamos perdiditos, perdiditos, perdiditos, perdiditos... >.<

--- src/js/entities.js
+++ src/js/entities.js
@@ -1,4 +1,40 @@
 'use strict';
+
+function Objeto (posicion, graphic, game){
+  this._posicion = posicion;
+  this._graphic = graphic;
+  this._game = game;
+  this._popo = game.add.sprite(posicion.x, posicion.y, _graphic);
+
+}
+Objeto.prototype.constructor === Objeto;
+
+
+function MoveObjects (posicion, velocidad, direccion, graphic, game){
+  Objeto.apply(this, [posicion, graphic, game]);
+  this._velocidad = velocidad;
+  this._direccion = direccion;
+}
+MoveObjects.prototype = Object.create(Objeto.prototype);
+MoveObjects.prototype.constructor === MoveObjects;
+
+function Popo (posicion, velocidad, direccion, graphic, game){
+  MoveObjects.apply(this, [posicion, velocidad, direccion, graphic, game]);
+  this._vidas = 4;
+  this._fuerzaSalto = -500;
+}
+Popo.prototype = Object.create(MoveObjects.prototype);
+Popo.prototype.constructor === Popo;
+
+
+function Platform (){}
+
+function Bonus(){}
+
+function Timer(){}
+
+
+
 function Popo (game, x,y){
 	this._popo = game.add.sprite(x,y,'popo');
 
